efb96503a822ca3dd2e11c7ae74494848b3cb4b5
Matthew Morse <mmorse1217@gmail.com>
2020-03-18 11:03:13 -0400

    adding lapackconfing.cmake from Kitware.

--- cmake/lapackConfig.cmake
+++ cmake/lapackConfig.cmake
@@ -1,3 +1,5 @@
+# From https://github.com/Kitware/CMake/blob/master/Modules/FindLAPACK.cmake
+
 # Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
 # file Copyright.txt or https://cmake.org/licensing for details.
 
